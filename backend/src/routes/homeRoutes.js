1961ae53c5a0c6f6cbbe6fa58238b4d1b408e194
Jesse-Tingle <jtingle0@gmail.com>
2026-06-13 18:31:29 -0400

    feat: add home creation endpoint

--- backend/src/routes/homeRoutes.js
+++ backend/src/routes/homeRoutes.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
-const { getHome, getHomes, getHomeById, getAssetsByHomeId } = require("../controllers/homeController.js");
+const {
+	getHome,
+	getHomes,
+	getHomeById,
+	getAssetsByHomeId,
+	createHome
+} = require("../controllers/homeController.js");
 
 router.get("/", getHomes);
+router.post("/", createHome);
+
 router.get("/:id/assets", getAssetsByHomeId)
 router.get("/:id", getHomeById);
 
