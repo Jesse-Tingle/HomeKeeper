12a4e8ea232ac4d242270aacf32778c73af5674b
Jesse-Tingle <jtingle0@gmail.com>
2026-06-19 17:15:51 -0400

    feat: add Zod validation for asset endpoints

--- backend/src/routes/assetRoutes.js
+++ backend/src/routes/assetRoutes.js
@@ -13,10 +13,12 @@ const {
 } = require("../controllers/assetController");
 
 const authenticateUser = require("../middleware/authMiddleware");
+const validateRequest = require("../middleware/validateRequest");
+const {createAssetSchema, updateAssetSchema} = require("../validators/assetSchemas");
 
 router.get("/:id/maintenance-events", authenticateUser, getMaintenanceEventsByAssetId);
 
-router.post("/", authenticateUser, createAsset);
+router.post("/", authenticateUser, validateRequest(createAssetSchema), createAsset);
 router.post("/maintenance-events", authenticateUser, createMaintenanceEvent);
 router.post("/maintenance-events", authenticateUser, createMaintenanceEvent);
 
@@ -24,7 +26,7 @@ router.put("/maintenance-events/:id", authenticateUser, updateMaintenanceEvent);
 router.delete("/maintenance-events/:id", authenticateUser, deleteMaintenanceEvent);
 router.delete("/:id", authenticateUser, deleteAsset);
 
-router.put("/:id", authenticateUser, updateAsset);
+router.put("/:id", authenticateUser, validateRequest(updateAssetSchema), updateAsset);
 
 router.get("/:id", authenticateUser, getAssetById);
 
