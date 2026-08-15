0694a9c19b8b55543e11a7a69fa1e8660a9f132d
Jesse-Tingle <jtingle0@gmail.com>
2026-08-15 12:34:31 -0400

    feat(database): add DATABASE_URL support

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,12 +1,18 @@
 const { Pool } = require("pg");
 const env = require("./env");
 
-const pool = new Pool({
-  host: env.DB_HOST,
-  port: env.DB_PORT,
-  database: env.DB_NAME,
-  user: env.DB_USER,
-  password: env.DB_PASSWORD,
-});
+const databaseConfig = env.DATABASE_URL
+  ? {
+    connectionString: env.DATABASE_URL,
+  }
+  : {
+    host: env.DB_HOST,
+    port: env.DB_PORT,
+    database: env.DB_NAME,
+    user: env.DB_USER,
+    password: env.DB_PASSWORD,
+  };
+
+const pool = new Pool(databaseConfig);
 
 module.exports = pool;
